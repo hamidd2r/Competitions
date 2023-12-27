80fd76dcef37feae2138b9dcc70259c9190d066f
hamidd2r <d2rt.hamid.ali@gmail.com>
2023-12-27 16:55:25 +0530

    add

---
+++ src/components/TopSlide12.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Topslider12 = () => {
+  return (
+    <div>
+     <>
+  <div className="container-fluid no-padding page-banner">
+    {/* Container */}
+    <div className="container">
+      {/* Banner Heading */}
+      <div className="banner-heading">
+        <h3>PetList</h3>
+        <ol className="breadcrumb">
+          <li>
+            <a href="#">pet List</a>
+          </li>
+          {/* <li className="active">Pets about</li> */}
+        </ol>
+      </div>
+      {/* Banner Heading /- */}
+    </div>
+    {/* Container /- */}
+  </div>
+  {/* Page Banner /- */}
+</>
+
+    </div>
+  )
+}
+
+export default Topslider12
